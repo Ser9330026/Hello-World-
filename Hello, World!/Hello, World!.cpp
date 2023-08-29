eaf4509aeb01c136e83f941e5e21396cc59fe2ed
Сybermart <ybermart@CYBERMART>
2023-08-29 21:58:11 +0500

    Добавьте файлы проекта.

---
+++ Hello, World!/Hello, World!.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	std::cout << "Меня зовут Сергей Бондаренко." << std::endl;
+	std::cout << "Мне " << 43 << " года." << std::endl;
+
+	return 0;
+}
